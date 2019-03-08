fb50e350d6a92e365e7e2fe4104c1b2215759a56
David Flaten <dflaten@amazon.com>
2019-03-08 15:44:14 -0800

    adding new program

---
+++ countbob.py
@@ -0,0 +1,22 @@
+import pdb
+#String -> String
+#counts instances of 'bob'
+
+#def bobcount(string):
+#    return 5
+
+
+def bobcount(scheck):
+    #pdb.set_trace()
+    slength = len(scheck)
+    bobs = 0
+    for a in range(0, slength - 3):
+        print(scheck[a:a+3])
+        if scheck[a:a+3] == "bob":
+            bobs += 1
+    return bobs
+
+print("Number of times bob occurs is: " + str(bobcount("bob")))
+
+def test_answer():
+    assert bobcount("bobabobdkfjbob") == 3
