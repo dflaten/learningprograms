eca18feb86815a7bf1fafc0f0813cad3582c450a
discocorg <david.flaten@gmail.com>
2019-03-08 21:59:32 -0800

    started alphastring

---
+++ alphastring.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import pdb
+
+#String -> String
+#Provide longest alphabetical substring from string
+#def CountAlphabeticalString(String):
+#    return SubString
+s = "aemdnvxoiernxkcv"
+slength = len(s)
+longastring = ""
+a = 0
+for a in range(0, slength):
+    #if ord(s[a]) < ord(s[a+1]):
+    #    
+    if any (c == s[a] for c in ("a", "e", "i", "o", "u")):
+        nvowels += 1
+    
+print("Number of vowels: " + str(nvowels))
+def test_answer():
+    assert CountAlphabeticalString("bobabobdkfjbob") == "abo"
