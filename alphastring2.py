d53274cafcd944e10287cf85b4c3774c6886ff7c
discocorg <david.flaten@gmail.com>
2019-03-10 10:45:43 -0700

    alaphastring2 is runnable but broken

--- alphastring2.py
+++ alphastring2.py
@@ -4,26 +4,29 @@
 #def AlphabeticalString(String):
 #    return SubString
 def main():
-    s = "aemdnvxoiernxkcv"
+    s = "aemdnvxabcdoiernxkcv"
     slength = len(s)
-    a = 0
     b = 0
     longastring = ""
+    guess = ""
     #Creates a guess char
-    def GuessMaker(starchar, ):
-        for a in range(a, slength-1):
-            first = ord(s[a])
-            second = ord(s[a+1])
-            import pdb; pdb.set_trace()
+    def GuessMaker(starchar, stringevaluated):
+        a = 0
+        guessinprogress = ""
+        guesslength = len(stringevaluated)
+        for a in range(a, guesslength-1):
+            first = ord(stringevaluated[a])
+            second = ord(stringevaluated[a+1])
+            #import pdb; pdb.set_trace()
             if first < second:
-                guess += s[a+1]
-            else 
-                return guess
+                guessinprogress += stringevaluated[a+1]
+            else: 
+                return guessinprogress
     #iterate through entire string
     for b in range (b, slength):
-        guess = GuessMaker(s[a], s)
+        guess = GuessMaker(s[b], s)
         if len(longastring) < len(guess):
-            longastring = guess''
+            longastring = guess
 
     print("The longest alphabetical string is: " + longastring)
     def test_answer():
