43b756a8f2ccd96a08591467be7614635ca27371
discocorg <david.flaten@gmail.com>
2019-03-10 16:02:51 -0700

    variables initialzed for creditcardbalance

---
+++ creditcardbalance.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#Float -> Float
+#Given the balance, annualInterestRate, and monthlyPaymentRate calculate the Remaining balance  
+def main():
+float balance = 10,000.00
+float annualInterestRate = .28
+float monthlPaymentRate = .15
+float monthlyInterestRate = annualInterestRate/12
+
+float minmonthlypayment = monthlyPaymentRate * previousbalance
+float monthlyUnpaidBalance = PreviousBalance - minmonthly payment
+float updatedbalanceEachMonth =  (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance) 
+index = 0
+
+float previousbalance = balance
+
+for index in range(0,12):
+    
+
+if __name__ == "__main__":
+    main()
